4f9cabee11ce0a2f2f43416ee6286bd83fc7aa61
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2021-05-06 08:43:11 +0300

    Update Ubuntu_x86_64_amd64.sh

--- Ubuntu_x86_64_amd64.sh
+++ Ubuntu_x86_64_amd64.sh
@@ -13,4 +13,4 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
  sudo apt-get update
- sudo apt-get install docker-compose docker-ce docker-ce-cli containerd.io
+ sudo apt-get install docker-compose docker-ce docker-ce-cli containerd.io -y 
